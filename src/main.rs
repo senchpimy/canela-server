d1f80e4a04d88e76a5c8d3b006c8974cc5733c25
senchpimy <senchpimy@gmail.com>
2024-06-14 11:18:20 -0600

    managing connections

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,25 @@
 use clap::Parser;
 use rusqlite::{Connection, Result};
 use serde::{Deserialize, Serialize};
-use tokio::io::AsyncReadExt;
-use tokio_tungstenite::tungstenite::accept;
 use uuid::Uuid;
-use warp::Filter;
+use warp::{filters::header::header, Filter};
 
 ////
 use std::{
     collections::HashMap,
-    env,
-    io::Error as IoError,
-    net::SocketAddr,
-    sync::{Arc, Mutex},
+    sync::{Arc, Mutex, RwLock},
 };
 
-use futures_channel::mpsc::{unbounded, UnboundedSender};
-use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
+use futures_util::{lock::Mutex, FutureExt, StreamExt};
 
-use tokio::net::{TcpListener, TcpStream};
-use tokio_tungstenite::tungstenite::protocol::Message;
+type ValidConnections<S: Into<String>> = Arc<Mutex<HashMap<S, S>>>;
+
 ////
 ///
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, default_value_t = 3030)]
+    #[arg(long, default_value_t = 3030)]
     port: u16,
 }
 
@@ -48,54 +42,77 @@ struct DbSearch {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let args = Args::parse();
-    //let connection = connect_to_db()?;
-    //let conn = Arc::new(Mutex::new(connection));
-    //let connection_req = move |val: ConnectionAttempt| validate_connection(val, Arc::clone(&conn));
-    //let hello = warp::path("connect")
-    //    .and(warp::body::json())
-    //    .map(connection_req);
+    let args = Args::parse(); //Pass trough a Rwlock
+    let connection = connect_to_db()?;
+    let valid_connections: ValidConnections<String> = Arc::new(Mutex::new(HashMap::new()));
+    let conn = Arc::new(Mutex::new(connection));
+    let connection_req = move |val: ConnectionAttempt| {
+        validate_connection(val, Arc::clone(&conn), Arc::clone(&valid_connections))
+    };
+    let hello = warp::path("connect")
+        .and(warp::body::json())
+        .map(connection_req);
 
-    //warp::serve(hello).run(([127, 0, 0, 1], args.port)).await;
-    //let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-    //while let Ok((stream, _)) = listener.accept().await {
-    //    tokio::spawn(handle_connection(stream));
-    //}
-    //
-    //    warp::serve(hello).run(([127, 0, 0, 1], args.port)).await;
-    let server = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-    println!("Server listening on 127.0.0.1:8080");
-    while let Ok((stream, addr)) = server.accept().await {
-        tokio::spawn(handle_connection(stream, addr));
-    }
+    let ws = warp::path("ws")
+        .and(warp::ws())
+        .and(warp::header("Custom-Header"))
+        .map(|ws: warp::ws::Ws, header_rx: String| {
+            println!("{}", header_rx);
+            ws.on_upgrade(move |socket| handle_connection(socket))
+        });
+
+    let routes = warp::get().and(hello.or(ws));
+    warp::serve(routes).run(([127, 0, 0, 1], args.port)).await;
+    //let warp_server = warp::serve(hello).run(([127, 0, 0, 1], args.http_port));
+    //tokio::spawn(warp_server);
 
     Ok(())
 }
 
-async fn handle_connection(raw_stream: tokio::net::TcpStream, addr: SocketAddr) {
-    println!("Incoming TCP connection from: {}", addr);
+async fn handle_connection(websocket: warp::ws::WebSocket) {
+    let connection_not_valid = true;
+    if connection_not_valid {
+        let _ = websocket.close().await;
+        return;
+    }
+    let (mut tx, mut rx) = websocket.split();
+    let mut index = 0;
+    loop {
+        //let m = warp::filters::ws::Message::text(format!("AAAA {}", index));
+        //match tx.send(m).await {
+        //    Ok(_) => {} //Enviado con exito
+        //    Err(_) => {
+        //        let _ = tx.close().await;
+        //    } //Error al enviar
+        //};
+        //index += 1;
 
-    let mut ws_stream = tokio_tungstenite::accept_async(raw_stream)
-        .await
-        .expect("Error during the websocket handshake occurred");
-    let (tx, rx) = unbounded();
-    let (outgoing, incoming) = ws_stream.split();
-
-    let broadcast_incoming = incoming.try_for_each(|msg| {
-        println!(
-            "Received a message from {}: {}",
-            addr,
-            msg.to_text().unwrap()
-        );
-        future::ok(())
-    });
-    pin_mut!(broadcast_incoming);
-    let receive_from_others = rx.map(Ok).forward(outgoing);
-    future::select(broadcast_incoming, receive_from_others).await;
-    println!("{} disconnected", &addr);
+        match rx.next().now_or_never() {
+            Some(val) => match val {
+                Some(v) => {
+                    println!("Recived Message");
+                    match v {
+                        Ok(v) => {
+                            dbg!(v);
+                        }
+                        Err(_) => {}
+                    }
+                    loop {}
+                }
+                None => {}
+            },
+            None => {
+                println!("No hay valor q leer");
+            }
+        };
+    }
 }
 
-fn validate_connection(data: ConnectionAttempt, db: Arc<Mutex<Connection>>) -> impl warp::Reply {
+fn validate_connection(
+    data: ConnectionAttempt,
+    db: Arc<Mutex<Connection>>,
+    valid_conn: ValidConnections<String>,
+) -> impl warp::Reply {
     let server_require_password = true; //TODO Get from config
     let server_can_generate_new_tokens = true; //TODO Get from config
     let server_password = String::from(""); //TODO Get from config
@@ -105,6 +122,7 @@ fn validate_connection(data: ConnectionAttempt, db: Arc<Mutex<Connection>>) -> i
         }
     }
     let r = db.lock().unwrap();
+    //Case where user is not registered, so we add them to the whitelist and send back the token
     if data.token.len() == 0 && server_can_generate_new_tokens {
         let token = Uuid::new_v4();
         let res = r.execute(
@@ -124,8 +142,11 @@ fn validate_connection(data: ConnectionAttempt, db: Arc<Mutex<Connection>>) -> i
         let response = NewUserResgistered {
             token: token.to_string(),
         };
+        add_valid_connection(valid_conn);
         return Ok(warp::reply::json(&response)); //reply the token and connections left
     }
+
+    //Case where the user is registered
     let mut search = r
         .prepare("SELECT connections_left,id FROM users WHERE token=?")
         .unwrap();
@@ -144,6 +165,7 @@ fn validate_connection(data: ConnectionAttempt, db: Arc<Mutex<Connection>>) -> i
         let response = NewUserResgistered {
             token: "Connection stablished".to_string(),
         };
+        add_valid_connection(valid_conn);
         return Ok(warp::reply::json(&response)); //reply the token and connections left
     }
     Err("Unreachable")
@@ -158,5 +180,25 @@ fn connect_to_db() -> Result<Connection, rusqlite::Error> {
                   connections_left INTEGER NOT NULL)",
         [],
     )?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS undelivered_messages (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    message TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id));",
+        [],
+    )?;
     Ok(conn)
+}
+
+fn add_valid_connection(valid_conn: ValidConnections<String>) {
+    loop {
+        match valid_conn.lock() {
+            Ok(mut conn) => {
+                conn.insert("".into(), "".into());
+                break;
+            }
+            Err(_) => {}
+        }
+    }
 }
